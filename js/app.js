824fe4dd2c450a16b78094a6543dec518b3e508e
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-09-27 00:25:28 +0000

    added image DOM References

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// >>> Create GLOBAL variables <<<
+let voteCount = 25;
+let products = [];
+
+// >>> DOM Manipulation <<<
+let imgSection = document.getElementById('img-section');
+let imgOne = document.getElementById('img1');
+let imgTwo = document.getElementById('img2');
+let imgThree = document.getElementById('img3');
+
