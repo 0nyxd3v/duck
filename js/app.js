a7ec8d76b5b66b4ed168cdd70c0faa666dadbbc4
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-09-27 00:44:07 +0000

    created constructors

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 // >>> Create GLOBAL variables <<<
 let voteCount = 25;
-let products = [];
+let productsArr = [];
 
 // >>> DOM Manipulation <<<
 let imgDiv = document.getElementById('img-div');
@@ -11,4 +11,14 @@ let imgTwo = document.getElementById('img2');
 let imgThree = document.getElementById('img3');
 
 let resultsBtn = document.getElementById('results-btn');
-let ulContainer = document.getElementById('ulList');
+let ulContainer = document.getElementById('ulList'); // resultsContainer
+
+// >>> Create Constructors <<<
+function Products(name, fileExtension = 'jpg') {
+  this.name = name;
+  this.img = `img/${name}.${fileExtension}`;
+  this.views = 0;
+  this.clicks = 0;
+
+  productsArr.push(this);
+}
