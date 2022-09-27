5b12c7801609cacdaed625ee4485e9b2ae7a3fe4
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-09-27 00:31:20 +0000

    created and grabbing results-btn element

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ let voteCount = 25;
 let products = [];
 
 // >>> DOM Manipulation <<<
-let imgSection = document.getElementById('img-section');
+let imgDiv = document.getElementById('img-div');
 let imgOne = document.getElementById('img1');
 let imgTwo = document.getElementById('img2');
 let imgThree = document.getElementById('img3');
 
+let resultsBtn = document.getElementById('results-btn');
