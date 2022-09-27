6aca10d84b12cd16df803377dbc16936f179ed06
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-09-27 00:56:20 +0000

    created randImg() that generates ranom images

--- js/app.js
+++ js/app.js
@@ -22,3 +22,22 @@ function Products(name, fileExtension = 'jpg') {
 
   productsArr.push(this);
 }
+
+// >>> Create HELPER functions <<<
+
+// function that generates random index of productsArr
+function randIdx() {
+  return Math.floor(Math.random() * productsArr.length);
+}
+
+// function that generates random images
+function randImg() {
+  let imgOneIdx = randIdx();
+  let imgTwoIdx = randIdx();
+  let imgThreeIdx = randIdx();
+
+  // .img >>> contains the filename of images, ex. bag.jpg
+  imgOne.src = productsArr[imgOneIdx].img;
+  imgTwo.src = productsArr[imgTwoIdx].img;
+  imgThree.src = productsArr[imgThreeIdx].img;
+}
